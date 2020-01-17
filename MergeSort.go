787f60eb7f6a428c828b8c136c05420da59eb0dc
David Brungardt <brungardtdb@gmail.com>
2020-01-17 15:12:51 -0700

    MergeSort.go Merge Sort Algorithm, initial commit.

---
+++ MergeSort.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	mySlice := []int{5, 3, 8, 1, 6, 10}
+	fmt.Println(mySlice)
+
+	fmt.Println(len(mergeSort(mySlice)))
+}
+
+func mergeSort(param []int) []int {
+
+	inputSlice := param
+	return inputSlice
+}
+
+func merge() {
+
+}
+
+func sort() {
+
+}
